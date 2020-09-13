b9035303485cb86df71e40b393be49068d372489
Harry Vella-Thompson <harrytphone@gmail.com>
2020-09-13 13:17:31 +0100

    Adding base transcription engine files

---
+++ AWSAPI.py
@@ -0,0 +1,49 @@
+import time
+import boto3
+
+# Create an S3 client
+s3 = boto3.client('s3')
+
+# Call S3 to list current buckets
+response = s3.list_buckets()
+
+# Get a list of all bucket names from the response
+buckets = [bucket['Name'] for bucket in response['Buckets']]
+
+# Print out the bucket list
+print("Bucket List: %s" % buckets)
+
+
+def transcribe_file(job_name, file_uri, transcribe_client):
+    transcribe_client.start_transcription_job(
+        TranscriptionJobName=job_name,
+        Media={'MediaFileUri': file_uri},
+        MediaFormat='wav',
+        LanguageCode='en-US'
+    )
+
+    max_tries = 60
+    while max_tries > 0:
+        max_tries -= 1
+        job = transcribe_client.get_transcription_job(TranscriptionJobName=job_name)
+        job_status = job['TranscriptionJob']['TranscriptionJobStatus']
+        if job_status in ['COMPLETED', 'FAILED']:
+            print(f"Job {job_name} is {job_status}.")
+            if job_status == 'COMPLETED':
+                print(
+                    f"Download the transcript from\n"
+                    f"\t{job['TranscriptionJob']['Transcript']['TranscriptFileUri']}.")
+            break
+        else:
+            print(f"Waiting for {job_name}. Current status is {job_status}.")
+        time.sleep(10)
+
+
+def main():
+    transcribe_client = boto3.client('transcribe')
+    file_uri = 's3://test-transcribe/answer2.wav'
+    transcribe_file('Example-job', file_uri, transcribe_client)
+
+
+if __name__ == '__main__':
+    main()
